a9f98f61f1871a4538cd76b56bb0a31362dd7689
Etienne Hoven <etienne.hoven@mvise.de>
2020-12-18 16:02:35 +0100

    init backend + frontend

---
+++ src/main/java/com/tech/wizard/techwizard/TechwizardApplication.java
@@ -0,0 +1,13 @@
+package com.tech.wizard.techwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechwizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechwizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/wizard/techwizard/TechwizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.wizard.techwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechwizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
