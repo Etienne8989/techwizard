dc921069c7e1c462b61af4b3b155e0d49fb38bfb
Etienne Hoven <etienne.hoven@mvise.de>
2021-04-19 14:40:09 +0200

    added blog module with routing

--- ui/src/app/pages/blog/blog.module.ts
+++ ui/src/app/pages/blog/blog.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BlogHomeComponent } from './blog-home/blog-home.component';
 
+import { BlogRoutingModule } from './blog-routing.module';
+import { BlogHomeComponent } from './blog-home/blog-home.component';
 
 
 @NgModule({
   declarations: [BlogHomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    BlogRoutingModule
   ]
 })
 export class BlogModule { }
