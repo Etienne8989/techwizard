e91b91b0a92b13a4e2343eb4899465d37cb8f0a9
Etienne Hoven <etienne.hoven@mvise.de>
2021-04-19 14:31:22 +0200

    display none for side nav because it should be only used for mobile

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -7,12 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {DashbordMainModule} from "./pages/dashbord-main/dashbord-main.module";
 import {HeaderModule} from "./core/modules/header/header.module";
 import {SidebarModule} from "./core/modules/sidebar/sidebar.module";
-import { BlogComponent } from './pages/blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogComponent,
   ],
   imports: [
     BrowserModule,
---
+++ ui/src/app/pages/blog/blog.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogHomeComponent } from './blog-home/blog-home.component';
+
+
+
+@NgModule({
+  declarations: [BlogHomeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlogModule { }
