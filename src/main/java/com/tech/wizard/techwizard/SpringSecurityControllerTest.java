4e4a95018a38ee8f50aa3a800844fae55f8e3f22
Etienne Hoven <etienne.hoven@mvise.de>
2021-04-19 14:07:01 +0200

    added controler

--- src/main/java/com/tech/wizard/techwizard/SpringSecurityControllerTest.java
+++ src/main/java/com/tech/wizard/techwizard/SpringSecurityControllerTest.java
@@ -1,5 +1,24 @@
 package com.tech.wizard.techwizard;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+//https://spring.io/guides/tutorials/spring-security-and-angular-js/
+
+@RestController
 public class SpringSecurityControllerTest {
+
+
+    @RequestMapping("/resource")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<String,Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
 
 }
